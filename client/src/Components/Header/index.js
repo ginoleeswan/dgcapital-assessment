255cbf4c43b656226d3a3b67b5c94e6dd3f2ea77
Gino Swanepoel <ginoleemusic@live.com>
2021-11-27 13:37:20 +0200

    Changed title of navbar

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="header-nav">
       <Container>
-        <Navbar.Brand href="/">DG CAPITAL assesment</Navbar.Brand>
+        <Navbar.Brand href="/">DG CAPITAL Project assesment</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
